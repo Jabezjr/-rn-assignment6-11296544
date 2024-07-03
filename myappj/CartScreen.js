8310825a9c0b27bf5b22043bf94a2ff694975911
Jabez <jt@BANGBRO.local>
2024-07-03 21:11:15 +0000

    Created the homepage of my app

---
+++ myappj/CartScreen.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CartScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>HElloo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
